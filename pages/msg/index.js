7d08f299670de4ac73b9ee512da55b1ef0ec62ad
samuelandkevin <YHIOS002@samuel.local>
2018-06-19 17:59:30 +0800

    修改聊天列表样式

--- pages/msg/index.js
+++ pages/msg/index.js
@@ -17,6 +17,20 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
     that = this;
     this._requestChatList(this.data.cursor, {
       success: function (ret) {
@@ -48,20 +62,6 @@ Page({
 
       }
     });
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
   },
 
   /**
