a1467dc771933dba7fe89df14624bc92a364e87f
samuelandkevin <YHIOS002@samuel.local>
2018-06-26 17:59:42 +0800

    修聊天模块

--- pages/msg/chatSingle.js
+++ pages/msg/chatSingle.js
@@ -48,7 +48,7 @@ Page({
               }
 
               
-              item.imContent = that.imContent(item.msgContent || "&nbsp;", item.msgType) 
+              item.imContent = that.imContent(item) 
               
 
 
@@ -86,37 +86,48 @@ return arr;
 	 * 转换内容 
 	 * @param {Object} content
 	 */
-  imContent:function (content, type) {
+  imContent:function (item) {
     var that = this;
+    var type    = item.msgType;
+    var content = item.msgContent || "&nbsp;";
     //支持的html标签
     var html = function (end) {
       return new RegExp('\\n*\\[' + (end || '') + '(pre|div|p|table|thead|th|tbody|tr|td|ul|li|ol|li|dl|dt|dd|h2|h3|h4|h5)([\\s\\S]*?)\\]\\n*', 'g');
     };
-    content = (content || '')
-      .replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/'/g, '&#39;').replace(/"/g, '&quot;') //XSS
+    content = (content || '').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/'/g, '&#39;').replace(/"/g, '&quot;') //XSS
       .replace(/@(\S+)(\s+?|$)/g, '@<a href="javascript:;">$1</a>$2') //转义@
-      .replace(/(http:\/\/|https:\/\/)((\w|=|\?|\.|\/|&|-|%)+)/g, function (url) {
+      .replace(/(http:\/\/|https:\/\/)((\w|=|\?|\.|\/|&|-|%)+)/g,
+      function (url) {
         if (type == 0) {
           return '<a href="javascript:;" data-web="' + url + '" style="text-decoration:underline;">' + url + '</a>';
         }
         return url;
-      })//转义网址
-      .replace(/&(?!#?[a-zA-Z0-9]+;)/g, '&amp;')
-      .replace(/\s{2}/g, '&nbsp;') //转义空格
-      .replace(/img\[([^\s]+?)\]/g, function (img) {  //转义图片
-        return '<img class="layui-layim-photos" src="' + img.replace(/(^img\[)|(\]$)/g, '') + '">';
-      })
-      .replace(/voice\[([^\s]+?)\]/g, function (voice) {  //转义语音
-        return '<a class="layui-layim-voice" href="javascript:;" data-url="' + voice.replace(/(^voice\[)|(\]$)/g, '') + '"></a>';
-      })
-      .replace(/file\([\s\S]+?\)\[[\s\S]*?\]/g, function (str) { //转义文件
+      }) //转义网址
+      .replace(/&(?!#?[a-zA-Z0-9]+;)/g, '&amp;').replace(/\s{2}/g, '&nbsp;') //转义空格
+      .replace(/img\[([^\s]+?)\]/g,
+      function (img) { //转义图片
+        return '<img style="max-width: 100%;vertical-align: middle;"  class="layui-layim-photos" src="' + img.replace(/(^img\[)|(\]$)/g, '') + '" />';
+      }).replace(/voice\[([^\s]+?)\]/g,
+      function (voice) { //转义语音
+        var color = '#fff';
+        var cls = 'iconfont icon-voice1';
+        if (item.direction == 1) {
+          color = '#00bf8f';
+          cls = 'iconfont icon-voice';
+        }
+        return '<a style="color: ' + color + ';" class="' +  cls  + ' " href="javascript:;" data-url="' + voice.replace(/(^voice\[)|(\]$)/g, '') + '"></a>';
+      }).replace(/file\([\s\S]+?\)\[[\s\S]*?\]/g,
+      function (str) { //转义文件
         var href = (str.match(/file\(([\s\S]+?)\)\[/) || [])[1];
         var text = (str.match(/\)\[([\s\S]*?)\]/) || [])[1];
-        if (!href)
-          return str;
-        return '<a class="layui-layim-file" href="' + href + '" download="' + (text || '下载文件') + '" target="_blank"><i class="webim-iconfont">&#xe60b;</i><cite>' + (text || '下载文件') + '</cite></a>';
-      })
-      .replace(/checkin\([\s\S]+?\)\[[\s\S]*?\]\[[\s\S]*?\]/g, function (str) { //转义签到
+        if (!href) return str;
+        var color = '#fff';
+        if(item.direction == 1){
+            color = '#00bf8f';
+        }
+        return '<a style="color:' + color + ';display: block;text-align: center;" class="layui-layim-file" href="' + href + '" download="' + (text || '下载文件') + '" target="_blank"><i style="font-size: 80px;line-height: 80px;" class="iconfont icon-file-download2"></i><a style="display: block;line-height: 20px;font-size: 12px;">' + (text || '下载文件') + '</a></a>';
+      }).replace(/checkin\([\s\S]+?\)\[[\s\S]*?\]\[[\s\S]*?\]/g,
+      function (str) { //转义签到
         var bgImg = (str.match(/checkin\(([\s\S]+?)\)\[/) || [])[1];
         var text = (str.match(/\)\[([\s\S]*?)\]/) || [])[1];
         var address = (str.match(/\]\[([\s\S]*?)\]/) || [])[1];
@@ -133,15 +144,15 @@ return arr;
         }
 
         return checkinHtml += '</div><div class="checkin-tips"><i class="mui-icon mui-icon-location-filled"></i>签到</div>';
-      })
-      .replace(/face\[([^\s\[\]]+?)\]/g, function (face) {  //转义表情
+      }).replace(/face\[([^\s\[\]]+?)\]/g,
+      function (face) { //转义表情
         var alt = face.replace(/^face/g, '');
         return '<img alt="' + alt + '" title="' + alt + '" src="' + that.faces[alt] + '">';
-      }).replace(/a\([\s\S]+?\)\[[\s\S]*?\]/g, function (str) { //转义链接
+      }).replace(/a\([\s\S]+?\)\[[\s\S]*?\]/g,
+      function (str) { //转义链接
         var href = (str.match(/a\(([\s\S]+?)\)\[/) || [])[1];
         var text = (str.match(/\)\[([\s\S]*?)\]/) || [])[1];
-        if (!href)
-          return str;
+        if (!href) return str;
         return '<a href="' + href + '" target="_blank">' + (text || href) + '</a>';
       }).replace(html(), '\<$1 $2\>').replace(html('/'), '\</$1\>') //转移HTML代码
       .replace(/\n/g, '<br>') //转义换行 
